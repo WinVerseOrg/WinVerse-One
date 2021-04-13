c005403b42aeb3facebb94cd6a3f9a68b985d5ae
winverse1 <82332468+winverse1@users.noreply.github.com>
2021-04-14 02:30:30 +0530

    Update build-log.ps1

--- build-log.ps1
+++ build-log.ps1
@@ -1,6 +1,31 @@
-$a = 350
-do {
-    Write-Host $a
-    Sleep 60
-    $a--
-} while ($i -gt 0)
+@echo off
+:Start
+title OnlineCmag-Timer
+color 07
+echo Type in an amount of time (i.e Seconds)
+set /p time=
+color 07
+:loop
+cls
+ping localhost -n 2 >nul
+set /a time=%time%-1
+echo %time%
+if %time% EQU 0 goto Timesup
+goto loop
+:Timesup
+title Time Is Up!
+ping localhost -n 2 >nul
+ping localhost -n 2 >nul
+cls
+echo The Time is up! :-P
+pause
+cls
+echo Thanks for using our Application.
+pause
+goto Web
+goto Exit
+:Web
+start http://www.onlinecmag.com
+:Exit
+Exit
+goto Exit
